8cdd9162d4a325df57e392d3abd47137a85eb0b1
Sumeet123806 <56047659+Sumeet123806@users.noreply.github.com>
2022-02-17 15:20:40 +0530

    Delete ProfileBuilder directory

--- ProfileBuilder/src/main/java/com/example/MyFirstPorject/Repo/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.example.MyFirstPorject.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.MyFirstPorject.profile.User;
-
-public interface Repository extends JpaRepository<User,Integer> {
-
-}
